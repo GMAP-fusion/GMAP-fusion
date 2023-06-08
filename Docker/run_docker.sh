e12aa8b1ee813f467d1519ad881decc757712d47
Brian Haas <bhaas@broadinstitute.org>
2023-06-08 19:37:10 +0000

    updated docker for devel v5.0.0

--- Docker/run_docker.sh
+++ Docker/run_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run --rm -it -v `pwd`:`pwd` trinityctat/gmapfusion:latest $*
+docker run --rm -it -v `pwd`:`pwd` -v ${CTAT_GENOME_LIB}:/ctat_genome_lib  trinityctat/gmapfusion:latest $*
 
